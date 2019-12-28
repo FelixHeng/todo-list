6408a1a9add372229d83c38d6dc52beea6e5dd2a
Felix <flxheng@gmail.com>
2019-12-28 20:18:15 +0100

    add task and cancel button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,28 +1,39 @@
 import React from "react";
 import useInputState from "../hooks/useInputState";
+import useToggleState from "../hooks/useToggleState";
 
-import { Paper, TextField } from "@material-ui/core";
+import { Paper, TextField, Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
 function TodoForm({ addTodo }) {
   const [value, handleChange, reset] = useInputState("");
+  const [isAdding, toggle] = useToggleState(false);
   return (
     <Paper>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!value) return;
-          addTodo(value);
-          reset();
-        }}
-        style={{ padding: "1rem" }}
-      >
-        <TextField
-          value={value}
-          onChange={handleChange}
-          label="Add new todo"
-          fullWidth
-        />
-      </form>
+      {isAdding ? (
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            if (!value) return;
+            addTodo(value);
+            reset();
+            toggle();
+          }}
+          style={{ padding: "1rem" }}
+        >
+          <TextField
+            value={value}
+            onChange={handleChange}
+            label="Add new todo"
+          />
+          <Button onClick={toggle}>Cancel</Button>
+        </form>
+      ) : (
+        <Button onClick={toggle} style={{ padding: "1rem" }}>
+          <AddIcon style={{ padding: "1rem" }} />
+          Add task
+        </Button>
+      )}
     </Paper>
   );
 }
