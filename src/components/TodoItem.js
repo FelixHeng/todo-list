1631365f3d57e0fb5b94efc7062b927170e798d1
Felix <flxheng@gmail.com>
2020-01-04 18:48:04 +0100

    add category colors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,7 +7,10 @@ import {
   ListItemText,
   Checkbox,
   IconButton,
-  ListItemSecondaryAction
+  ListItemSecondaryAction,
+  makeStyles,
+  Button,
+  Typography
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
@@ -23,10 +26,46 @@ function Todo({
   editTodo
 }) {
   const [isEditing, toggle] = useToggleState(false);
-  console.log("cateeee", { category });
-  console.log("daaate", { date });
+  const workColors = "#3498DB";
+  const personalColors = "#E74C3C";
+  const socialColors = "#1ABC9C";
+  const readingColors = "#873600";
+  const lifeColors = "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)";
+  const othersColors = "#F1C40F";
+
+  let catColors;
+  if (category === "Work") {
+    catColors = workColors;
+  } else if (category === "Personal") {
+    catColors = personalColors;
+  } else if (category === "Social") {
+    catColors = socialColors;
+  } else if (category === "Reading") {
+    catColors = readingColors;
+  } else if (category === "Life Goals") {
+    catColors = lifeColors;
+  } else if (category === "Others") {
+    catColors = othersColors;
+  }
+
+  console.log("caaaat", catColors);
+  console.log("value", category);
+  const useStyles = makeStyles({
+    category: {
+      background: catColors,
+      border: 0,
+      borderRadius: 3,
+      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      color: "white",
+      pointerEvents: "none"
+      // textTransform: "none"
+    }
+  });
+  const classes = useStyles();
+  // console.log("cateeee", { category });
+  // console.log("daaate", { date });
   return (
-    <ListItem style={{ height: "64px" }}>
+    <ListItem>
       {isEditing ? (
         <EditTodoForm
           editTodo={editTodo}
@@ -44,10 +83,12 @@ function Todo({
             onClick={() => toggleTodo(id)}
           />
           <ListItemText
-            style={{ textDecoration: completed ? "line-through" : "none" }}
+            style={{
+              textDecoration: completed ? "line-through" : "none"
+            }}
           >
-            <div>{category}</div>
-            <div>{task}</div>
+            <Button className={classes.category}>{category}</Button>
+            <div style={{ width: "80%" }}>{task}</div>
             <div>{date}</div>
           </ListItemText>
           <ListItemSecondaryAction>
